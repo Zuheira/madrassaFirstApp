ac10b941879f5d1c1acbc6ca51aed5d1a9ae3a99
Zuheira <115471797+Zuheira@users.noreply.github.com>
2022-10-13 12:42:04 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>mussa</Text>
+
+			<Text>Zuheirah</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
